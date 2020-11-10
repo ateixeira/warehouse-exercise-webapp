8278bc890e4b46fe5457cc6b21e48158b4fa8813
Andre Teixeira <andre.teixeira@jumbo.com>
2020-11-10 18:18:37 +0100

    :construction: Fetch product data from database

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,52 @@ import { Panel, Layout } from "./screens";
 import { Menu } from "./components";
 import { Article, PanelTypes, Product } from "./types";
 import { useQuery } from "@apollo/client";
-import { GET_ARTICLES } from "./utils/queries";
+import { GET_ARTICLES, GET_PRODUCTS } from "./utils/queries";
 import "./app.scss";
 
-const products: Product[] = [
-  { id: "product1", name: "Product", price: 11.11, articles: [] },
-  { id: "product2", name: "Product", price: 22.22, articles: [] },
-  { id: "product3", name: "Product", price: 33.33, articles: [] },
-  { id: "product4", name: "Product", price: 44.44, articles: [] },
-  { id: "product5", name: "Product", price: 55.55, articles: [] },
-  { id: "product6", name: "Product", price: 66.66, articles: [] },
-  { id: "product7", name: "Product", price: 77.77, articles: [] },
-  { id: "product8", name: "Product", price: 88.88, articles: [] },
-];
 interface ArticleData {
   getAllArticles: ArticleList;
+}
+interface ProductData {
+  getAllProducts: ProductList;
 }
 interface ArticleList {
   articles: Article[];
 }
+interface ProductList {
+  products: Product[];
+}
 
 function App() {
-  const { loading, data } = useQuery<ArticleData, {}>(GET_ARTICLES, {});
+  const articles = useQuery<ArticleData, {}>(GET_ARTICLES, {});
+  const products = useQuery<ProductData, {}>(GET_PRODUCTS, {});
+
+  console.log("articles");
+  console.log(articles);
+  console.log("products");
+  console.log(products);
 
   return (
     <Router>
       <div className="app">
         <Menu />
-        {data && (
+        {articles.data && products.data && (
           <Layout>
             <Switch>
               <Route path="/articles">
                 <Panel
                   title="Articles"
                   type={PanelTypes.Article}
-                  loading={loading}
-                  items={data?.getAllArticles.articles}
+                  loading={articles.loading}
+                  items={articles.data?.getAllArticles?.articles || []}
                 />
               </Route>
               <Route path="/products">
                 <Panel
                   title="Products"
                   type={PanelTypes.Product}
-                  loading={loading}
-                  items={products}
+                  loading={products.loading}
+                  items={products.data?.getAllProducts?.products || []}
                 />
               </Route>
             </Switch>
--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -12,6 +12,23 @@ export const GET_ARTICLES = gql`
     }
   }
 `;
+export const GET_PRODUCTS = gql`
+  query getAllProducts {
+    getAllProducts {
+      products {
+        id
+        name
+        price
+        articles {
+          art_id
+          name
+          stock
+        }
+        updatedOn
+      }
+    }
+  }
+`;
 
 export const UPLOAD_FILE = gql`
   mutation uploadFile($file: Upload!) {
